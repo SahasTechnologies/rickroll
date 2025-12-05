03bb4d8ecd4625101a1eb1ee20e5fb3f7f084451
Sahas Shimpi <sahas@sahas.dpdns.org>
2025-12-05 11:44:17 +1100

    Update rickroll.ps1

--- rickroll.ps1
+++ rickroll.ps1
@@ -1,17 +1,12 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Media
-
-# Download the MP3 to a temp file
-$mp3Url = "https://github.com/SahasTechnologies/rickroll/raw/refs/heads/main/rickroll.mp3"
-$tempFile = [System.IO.Path]::Combine($env:TEMP, "rickroll.mp3")
-Invoke-WebRequest -Uri $mp3Url -OutFile $tempFile
+# Rickroll.ps1
+# Plays audio from GitHub and shows lyric popups
 
 # Use Windows Media Player COM object to play MP3
 $player = New-Object -ComObject WMPlayer.OCX
-$player.URL = $tempFile
+$player.URL = "https://github.com/SahasTechnologies/rickroll/raw/refs/heads/main/rickroll.mp3"
 $player.controls.play()
 
-# Show lyric popups
+# Popup lyrics
 $wshell = New-Object -ComObject WScript.Shell
 $lyrics = @(
     "Never gonna give you up",
