e8ffe7549f3521271aae7d94c911e6250cf9d367
Sahas Shimpi <sahas@sahas.dpdns.org>
2025-12-05 11:34:22 +1100

    Create rickroll.ps1

---
+++ rickroll.ps1
@@ -0,0 +1,15 @@
+$wshell = New-Object -ComObject WScript.Shell
+
+# Add your lyric lines here
+$lyrics = @(
+    "Never gonna give you up",
+    "Never gonna let you down",
+    "Never gonna run around and desert you",
+    "Never gonna make you cry",
+    "Never gonna say goodbye",
+    "Never gonna tell a lie and hurt you"
+)
+
+foreach ($line in $lyrics) {
+    $wshell.Popup($line,0,"Rickroll",64)
+}
